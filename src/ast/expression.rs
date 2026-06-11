22aba951b4cd63e552e9c61ca43071c28b75b68a
elysia <3302843873@qq.com>
2026-06-11 10:10:18 +0800

    added math ops to ast

--- src/ast/expression.rs
+++ src/ast/expression.rs
@@ -3,4 +3,17 @@
 pub enum Expression {
     Ident(String),
     Number(i64),
+    MathOp {
+        kind: MathOpKind,
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+pub enum MathOpKind {
+    Add,
+    Subtract,
+    Divide,
+    Multiply,
 }
