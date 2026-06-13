4858e1670cfd3613cead215b3efd1c87a957f9ba
elysia <3302843873@qq.com>
2026-06-13 18:18:12 +0800

    if statement codegen

--- src/codegen/statement.rs
+++ src/codegen/statement.rs
@@ -16,7 +16,32 @@ impl<'a> Codegen<'a> {
             Statement::Expression(expr) => {
                 _ = self.compile_expr(expr);
             }
-            Statement::If { cond, body } => todo!(),
+            Statement::If { cond, body } => {
+                let cond = self.compile_expr(cond).into_int_value();
+
+                let current_block = self.builder.get_insert_block().unwrap();
+                let current_function = current_block.get_parent().unwrap();
+
+                let then_block = self.ctx.append_basic_block(current_function, "if_then");
+                let end_block = self.ctx.append_basic_block(current_function, "if_end");
+
+                self.builder
+                    .build_conditional_branch(cond, then_block, end_block)
+                    .unwrap();
+
+                self.builder.position_at_end(then_block);
+
+                for stmt in &body.statements {
+                    self.compile_statement(stmt);
+                }
+
+                // Force jump to end_block if then_block dont have a return
+                if then_block.get_terminator().is_none() {
+                    self.builder.build_unconditional_branch(end_block).unwrap();
+                }
+
+                self.builder.position_at_end(end_block);
+            }
         };
     }
 }
