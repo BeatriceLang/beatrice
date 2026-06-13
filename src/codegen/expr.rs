fbe763b3109d0007db4e0ba5566de56886a80a6f
elysia <3302843873@qq.com>
2026-06-13 11:10:01 +0800

    codegen: ident expr

--- src/codegen/expr.rs
+++ src/codegen/expr.rs
@@ -33,7 +33,7 @@ impl<'a> Codegen<'a> {
                     .try_as_basic_value()
                     .unwrap_basic()
             }
-            _ => todo!(),
+            Expression::Ident(ident) => *self.values.get(ident).unwrap(),
         }
     }
 }
