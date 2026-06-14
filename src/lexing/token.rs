a72a51732aab068dcb98eaf85c354c049c79994e
elysia <3302843873@qq.com>
2026-06-14 23:59:24 +0800

    impl Display for Token

--- src/lexing/token.rs
+++ src/lexing/token.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use logos::Logos;
 
 #[derive(Logos, Debug, Clone, PartialEq, Eq)]
@@ -71,6 +73,36 @@ pub enum Token {
 
     #[token("let")]
     Let,
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Token::If => f.write_str("if"),
+            Token::Fn => f.write_str("fn"),
+            Token::LeftParen => f.write_str("("),
+            Token::RightParen => f.write_str(")"),
+            Token::LeftBrace => f.write_str("{"),
+            Token::RightBrace => f.write_str("}"),
+            Token::Colon => f.write_str(":"),
+            Token::Return => f.write_str("return"),
+            Token::Semicolon => f.write_str(";"),
+            Token::Comma => f.write_str(","),
+            Token::RetArrow => f.write_str("->"),
+            Token::I32 => f.write_str("i32"),
+            Token::Ident(name) => write!(f, "{name}"),
+            Token::Number(value) => write!(f, "{value}"),
+            Token::Add => f.write_str("+"),
+            Token::Minus => f.write_str("-"),
+            Token::Divide => f.write_str("/"),
+            Token::Multiply => f.write_str("*"),
+            Token::GreaterThan => f.write_str(">"),
+            Token::LessThan => f.write_str("<"),
+            Token::Equal => f.write_str("=="),
+            Token::Assign => f.write_str("="),
+            Token::Let => f.write_str("let"),
+        }
+    }
 }
 
 #[cfg(test)]
