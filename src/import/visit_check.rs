e7211ee8bef3b42798cdb3f86f7e41b79f980062
elysia <3302843873@qq.com>
2026-06-16 11:20:42 +0800

    nolonger error when already imported

--- src/import/visit_check.rs
+++ src/import/visit_check.rs
@@ -9,23 +9,10 @@ impl<'a> ImportProcessor<'a> {
     #[must_use]
     pub(super) fn visit_check(&mut self, path: PathBuf) -> bool {
         if self.visited.contains(&path) {
-            self.push_diagnostic(path);
             true
         } else {
             self.visited.push(path);
             false
         }
-    }
-
-    fn push_diagnostic(&mut self, path: PathBuf) {
-        let diag = Diagnostic {
-            // TODO
-            span: 0..0,
-            kind: DiagnosticKind::Error,
-            message: format!("Duplicate import of {path:?}"),
-            label: format!("{path:?} have already been imported before"),
-        };
-
-        self.diagnostics.push(diag);
     }
 }
