54b3bd15a5a63e4bfa29749e55eb6abbb53117f5
elysia <3302843873@qq.com>
2026-06-19 09:39:25 +0800

    kawaii: stuf

---
+++ kawaii/src/args.rs
@@ -0,0 +1,10 @@
+#[derive(clap::Parser, Debug)]
+pub struct Args {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(clap::Subcommand, Debug)]
+pub enum Command {
+    Build,
+}
--- kawaii/src/main.rs
+++ kawaii/src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+use crate::args::{Args, Command};
+
+mod args;
+mod build;
+
 fn main() {
-    todo!()
+    let arg = Args::parse();
+
+    match arg.command {
+        Command::Build => build::run(),
+    }
 }
