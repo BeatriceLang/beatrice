30fe12c3bb632daacb0ff1a9db6e37c13c93eb8e
elysia <3302843873@qq.com>
2026-06-19 13:37:43 +0800

    kawaii: run: finished i think

--- kawaii/src/run.rs
+++ kawaii/src/run.rs
@@ -1,5 +1,19 @@
+use std::env::current_dir;
+
 use anyhow::Result;
+use xshell::{Shell, cmd};
+
+use crate::{build::build, project_info::ProjectInfo};
 
 pub fn run() -> Result<()> {
+    let project_info = ProjectInfo::from_project_toml()?;
+
+    build(project_info.clone())?;
+
+    let output = current_dir()?.join("target").join(project_info.name);
+
+    let sh = Shell::new()?;
+    cmd!(sh, "{output}").run()?;
+
     Ok(())
 }
