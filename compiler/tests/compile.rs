275a27c025bd1ed4ad5594c6d34c8cd4adb5fc42
elysia <3302843873@qq.com>
2026-06-20 09:18:17 +0800

    compiler: int tests: added while tests

--- compiler/tests/compile.rs
+++ compiler/tests/compile.rs
@@ -243,6 +243,46 @@ fn compiles_var_assignment_from_previous_value() {
 }
 
 #[test]
+fn compiles_while_loop() {
+    let code = compile_and_run(
+        "while_loop",
+        "
+        fn main() -> i32 {
+            var value: i32 = 0;
+
+            while value < 42 {
+                value = value + 1;
+            }
+
+            return value;
+        }
+        ",
+    );
+
+    assert_eq!(code, Some(42));
+}
+
+#[test]
+fn compiles_while_loop_with_initially_false_condition() {
+    let code = compile_and_run(
+        "while_loop_initially_false",
+        "
+        fn main() -> i32 {
+            var value: i32 = 42;
+
+            while value < 42 {
+                value = 0;
+            }
+
+            return value;
+        }
+        ",
+    );
+
+    assert_eq!(code, Some(42));
+}
+
+#[test]
 fn compiles_puts_hello_world() {
     let output = compile_and_run_output(
         "puts_hello_world",
