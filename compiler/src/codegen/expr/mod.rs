bef2c02cf99dd9361e4e93f158190444f279ac9e
elysia <3302843873@qq.com>
2026-06-20 16:33:15 +0800

    compiler: codegen: expr: deref

--- compiler/src/codegen/expr/mod.rs
+++ compiler/src/codegen/expr/mod.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use crate::{
     ast::{Type, expression::Expression},
     codegen::{Codegen, utils::TypedValue},
@@ -32,7 +34,24 @@ impl<'a> Codegen<'a> {
                     .ok()?;
                 Some(TypedValue { inner: value, ty })
             }
-            Expression::Deref { ptr } => todo!(),
+            Expression::Deref { ptr } => {
+                let ptr = self.compile_expr(ptr).unwrap();
+                let Type::Ptr(pointed_ty) = ptr.ty else {
+                    panic!("Expected pointer");
+                };
+
+                let llvm_pointed_ty = self.to_llvm_type(&pointed_ty);
+
+                let value = self
+                    .builder
+                    .build_load(llvm_pointed_ty, ptr.inner.into_pointer_value(), "_")
+                    .unwrap();
+
+                Some(TypedValue {
+                    inner: value,
+                    ty: *pointed_ty,
+                })
+            }
             Expression::StringLiteral(string) => Some(TypedValue {
                 inner: self
                     .builder
