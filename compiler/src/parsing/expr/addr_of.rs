39f29ce8810a47c304c845aab5b8dc9f8cedf76e
elysia <3302843873@qq.com>
2026-06-20 16:49:27 +0800

    compiler: paser: expr: addr_of: unit test

--- compiler/src/parsing/expr/addr_of.rs
+++ compiler/src/parsing/expr/addr_of.rs
@@ -9,3 +9,43 @@ pub(super) fn addr_of_expr<'a>(expr: parser_type!(Expression)) -> parser_type!(E
             value: Box::new(value),
         })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        ast::expression::Expression,
+        parsing::{expr::expr, test_ident, test_parse, test_tokens},
+    };
+
+    #[test]
+    fn parses_addr_of_expr() {
+        let tokens = test_tokens![Token::AddressOf, Token::Ident("value".into())];
+
+        assert_eq!(
+            test_parse(addr_of_expr(expr()), &tokens),
+            Expression::AddressOf {
+                value: Expression::Ident(test_ident("value")).into(),
+            }
+        );
+    }
+
+    #[test]
+    fn parses_nested_addr_of_expr() {
+        let tokens = test_tokens![
+            Token::AddressOf,
+            Token::AddressOf,
+            Token::Ident("value".into()),
+        ];
+
+        assert_eq!(
+            test_parse(addr_of_expr(expr()), &tokens),
+            Expression::AddressOf {
+                value: Expression::AddressOf {
+                    value: Expression::Ident(test_ident("value")).into(),
+                }
+                .into(),
+            }
+        );
+    }
+}
