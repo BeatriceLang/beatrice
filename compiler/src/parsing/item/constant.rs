9c21dbe19db09bed41dac273acefaedfd0d6a0e8
elysia <3302843873@qq.com>
2026-06-24 20:19:45 +0800

    compiler: parser: item: const: unit tests

--- compiler/src/parsing/item/constant.rs
+++ compiler/src/parsing/item/constant.rs
@@ -1,4 +1,4 @@
-use chumsky::{Parser, primitive::just};
+use chumsky::{primitive::just, Parser};
 
 use crate::{
     ast::Item,
@@ -14,4 +14,94 @@ pub(super) fn constant<'a>() -> parser_type!(Item) {
         .then_ignore(just(Token::Assign))
         .then(expr())
         .map(|((name, ty), val)| Item::Const { name, ty, val })
+}
+
+#[cfg(test)]
+mod tests {
+    use chumsky::Parser as _;
+
+    use super::*;
+    use crate::{
+        ast::{expression::Expression, Type},
+        parsing::{test_ident, test_input, test_parse, test_tokens},
+    };
+
+    #[test]
+    fn parses_constant_item() {
+        let tokens = test_tokens![
+            Token::Const,
+            Token::Ident("answer".into()),
+            Token::Colon,
+            Token::I32,
+            Token::Assign,
+            Token::Number(42),
+        ];
+
+        assert_eq!(
+            test_parse(constant(), &tokens),
+            Item::Const {
+                name: test_ident("answer"),
+                ty: Type::I32,
+                val: Expression::Number(42),
+            }
+        );
+    }
+
+    #[test]
+    fn parses_constant_item_with_string_literal() {
+        let tokens = test_tokens![
+            Token::Const,
+            Token::Ident("message".into()),
+            Token::Colon,
+            Token::String,
+            Token::Assign,
+            Token::StringLiteral("hello".into()),
+        ];
+
+        assert_eq!(
+            test_parse(constant(), &tokens),
+            Item::Const {
+                name: test_ident("message"),
+                ty: Type::String,
+                val: Expression::StringLiteral("hello".into()),
+            }
+        );
+    }
+
+    #[test]
+    fn parses_constant_item_with_ptr_type() {
+        let tokens = test_tokens![
+            Token::Const,
+            Token::Ident("ptr".into()),
+            Token::Colon,
+            Token::Multiply,
+            Token::I32,
+            Token::Assign,
+            Token::Ident("value".into()),
+        ];
+
+        assert_eq!(
+            test_parse(constant(), &tokens),
+            Item::Const {
+                name: test_ident("ptr"),
+                ty: Type::Ptr(Box::new(Type::I32)),
+                val: Expression::Ident(test_ident("value")),
+            }
+        );
+    }
+
+    #[test]
+    fn rejects_constant_item_without_assign() {
+        let tokens = test_tokens![
+            Token::Const,
+            Token::Ident("answer".into()),
+            Token::Colon,
+            Token::I32,
+            Token::Number(42),
+        ];
+        let errors = constant().parse(test_input(&tokens)).into_errors();
+
+        assert_eq!(errors.len(), 1);
+        assert_eq!(errors[0].span().into_range(), 4..5);
+    }
 }
