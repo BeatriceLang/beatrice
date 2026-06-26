0328140eae5c3652b4ce75cac3360a540237ad43
elysia <3302843873@qq.com>
2026-06-26 10:53:00 +0800

    compiler: parser: expr: field_access: unit tests

--- compiler/src/parsing/expr/field_access.rs
+++ compiler/src/parsing/expr/field_access.rs
@@ -8,3 +8,29 @@ pub(super) fn field_access<'a>() -> parser_type!(Expression) {
         .then(ident())
         .map(|(base, field)| Expression::FieldAccess { base, field })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        ast::expression::Expression,
+        parsing::{test_ident, test_parse, test_tokens},
+    };
+
+    #[test]
+    fn parses_field_access() {
+        let tokens = test_tokens![
+            Token::Ident("point".into()),
+            Token::Dot,
+            Token::Ident("x".into()),
+        ];
+
+        assert_eq!(
+            test_parse(field_access(), &tokens),
+            Expression::FieldAccess {
+                base: test_ident("point"),
+                field: test_ident("x"),
+            }
+        );
+    }
+}
