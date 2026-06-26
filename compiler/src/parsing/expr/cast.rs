9f165b10b561e7179059d6ca755109e09dc652cb
elysia <3302843873@qq.com>
2026-06-27 07:00:27 +0800

    compiler: parser: expr: cast: fix

--- compiler/src/parsing/expr/cast.rs
+++ compiler/src/parsing/expr/cast.rs
@@ -3,10 +3,10 @@ use chumsky::{Parser, primitive::just};
 use crate::{ast::expression::Expression, lexing::token::Token, parsing::ty::ty};
 
 pub(super) fn cast<'a>(expr: parser_type!(Expression)) -> parser_type!(Expression) {
-    expr.then_ignore(just(Token::As))
-        .then(ty())
-        .map(|(value, to)| Expression::Cast {
+    expr.foldl(just(Token::As).ignore_then(ty()).repeated(), |value, to| {
+        Expression::Cast {
             value: Box::new(value),
             to,
-        })
+        }
+    })
 }
