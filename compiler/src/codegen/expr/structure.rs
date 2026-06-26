faa3950f10fdf545beb6f7a9e22c1b2028b8e4d1
elysia <3302843873@qq.com>
2026-06-26 08:08:09 +0800

    compiler: codegen: expr: struct

--- compiler/src/codegen/expr/structure.rs
+++ compiler/src/codegen/expr/structure.rs
@@ -1,5 +1,7 @@
+use inkwell::{types::BasicType, values::BasicValue};
+
 use crate::{
-    ast::{Ident, expression::Expression},
+    ast::{Ident, Type, expression::Expression},
     codegen::{Codegen, utils::TypedValue},
 };
 
@@ -9,6 +11,27 @@ impl<'a> Codegen<'a> {
         name: &Ident,
         fields: &[(Ident, Box<Expression>)],
     ) -> TypedValue<'a> {
-        todo!()
+        let struct_type = self
+            .structs
+            .get(name.as_str())
+            .unwrap()
+            .as_basic_type_enum();
+        let struct_ptr = self.builder.build_alloca(struct_type, "_").unwrap();
+
+        // TODO: resolve the proper order from the ident
+        for (i, (_, expr)) in fields.iter().enumerate() {
+            let field_ptr = self
+                .builder
+                .build_struct_gep(struct_type, struct_ptr, i.try_into().unwrap(), "_")
+                .unwrap();
+            let value = self.compile_expr(expr).unwrap();
+
+            self.builder.build_store(field_ptr, value.inner).unwrap();
+        }
+
+        TypedValue {
+            inner: struct_ptr.as_basic_value_enum(),
+            ty: Type::Struct(name.as_str().to_owned()),
+        }
     }
 }
