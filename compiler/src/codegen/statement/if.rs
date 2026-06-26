52d370af1e7694b00ec0c93dc045132b7d0a3d94
elysia <3302843873@qq.com>
2026-06-27 00:18:08 +0800

    compiler: codegen: stmt: if: added Bool assert

--- compiler/src/codegen/statement/if.rs
+++ compiler/src/codegen/statement/if.rs
@@ -1,11 +1,13 @@
 use crate::{
-    ast::{Block, expression::Expression},
+    ast::{Block, Type, expression::Expression},
     codegen::Codegen,
 };
 
 impl Codegen<'_> {
     pub(super) fn compile_if(&mut self, cond: &Expression, body: &Block) {
-        let cond = self.compile_expr(cond).unwrap().inner.into_int_value();
+        let cond = self.compile_expr(cond).unwrap();
+
+        assert!(matches!(cond.ty, Type::Bool));
 
         let current_block = self.builder.get_insert_block().unwrap();
         let current_function = current_block.get_parent().unwrap();
@@ -14,7 +16,7 @@ impl Codegen<'_> {
         let end_block = self.ctx.append_basic_block(current_function, "if_end");
 
         self.builder
-            .build_conditional_branch(cond, then_block, end_block)
+            .build_conditional_branch(cond.inner.into_int_value(), then_block, end_block)
             .unwrap();
 
         self.builder.position_at_end(then_block);
