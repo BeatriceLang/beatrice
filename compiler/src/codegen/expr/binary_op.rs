e1a0380e149cc8278fcd7861038afc5826e56739
elysia <3302843873@qq.com>
2026-06-26 15:00:12 +0800

    compiler: codegen: expr: binary_op: returns proper ty

--- compiler/src/codegen/expr/binary_op.rs
+++ compiler/src/codegen/expr/binary_op.rs
@@ -15,32 +15,39 @@ impl<'a> Codegen<'a> {
         kind: BinaryOpKind,
         rhs: &Expression,
     ) -> TypedValue<'a> {
-        let lhs = self.compile_expr(lhs).unwrap().inner.into_int_value();
-        let rhs = self.compile_expr(rhs).unwrap().inner.into_int_value();
+        let lhs = self.compile_expr(lhs).unwrap();
+        let rhs = self.compile_expr(rhs).unwrap();
+        let llvm_lhs = lhs.inner.into_int_value();
+        let llvm_rhs = rhs.inner.into_int_value();
+
+        assert_eq!(lhs.ty, rhs.ty);
 
         let value = match kind {
-            BinaryOpKind::Add => self.builder.build_int_add(lhs, rhs, "_").unwrap(),
-            BinaryOpKind::Subtract => self.builder.build_int_sub(lhs, rhs, "_").unwrap(),
-            BinaryOpKind::Divide => self.builder.build_int_signed_div(lhs, rhs, "_").unwrap(),
-            BinaryOpKind::Multiply => self.builder.build_int_mul(lhs, rhs, "_").unwrap(),
+            BinaryOpKind::Add => self.builder.build_int_add(llvm_lhs, llvm_rhs, "_").unwrap(),
+            BinaryOpKind::Subtract => self.builder.build_int_sub(llvm_lhs, llvm_rhs, "_").unwrap(),
+            BinaryOpKind::Divide => self
+                .builder
+                .build_int_signed_div(llvm_lhs, llvm_rhs, "_")
+                .unwrap(),
+            BinaryOpKind::Multiply => self.builder.build_int_mul(llvm_lhs, llvm_rhs, "_").unwrap(),
             BinaryOpKind::GreaterThan => self
                 .builder
-                .build_int_compare(IntPredicate::SGT, lhs, rhs, "_")
+                .build_int_compare(IntPredicate::SGT, llvm_lhs, llvm_rhs, "_")
                 .unwrap(),
             BinaryOpKind::LessThan => self
                 .builder
-                .build_int_compare(IntPredicate::SLT, lhs, rhs, "_")
+                .build_int_compare(IntPredicate::SLT, llvm_lhs, llvm_rhs, "_")
                 .unwrap(),
             BinaryOpKind::EqualTo => self
                 .builder
-                .build_int_compare(IntPredicate::EQ, lhs, rhs, "_")
+                .build_int_compare(IntPredicate::EQ, llvm_lhs, llvm_rhs, "_")
                 .unwrap(),
         }
         .into();
 
         TypedValue {
             inner: value,
-            ty: Type::I32,
+            ty: lhs.ty,
         }
     }
 }
