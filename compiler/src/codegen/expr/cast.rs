5d969f431a716189c9dcec9a36252860b27d8926
elysia <3302843873@qq.com>
2026-06-27 10:40:10 +0800

    compiler: codegen: expr: cast: removed int to bool

--- compiler/src/codegen/expr/cast.rs
+++ compiler/src/codegen/expr/cast.rs
@@ -17,16 +17,6 @@ impl<'a> Codegen<'a> {
                 .build_int_z_extend(from.inner.into_int_value(), llvm_to.into_int_type(), "_")
                 .unwrap()
                 .as_basic_value_enum(),
-            // int to bool
-            (Type::U32 | Type::I32, Type::Bool) => {
-                let value = from.inner.into_int_value();
-                let zero = value.get_type().const_zero();
-
-                self.builder
-                    .build_int_compare(IntPredicate::NE, value, zero, "_")
-                    .unwrap()
-                    .as_basic_value_enum()
-            }
             // int to int, or bool to bool
             (Type::U32 | Type::I32 | Type::Bool, Type::U32 | Type::I32 | Type::Bool) => self
                 .builder
