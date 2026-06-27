1fefe6f3b52217de0b26d45fde91b7b34c2e7a7d
elysia <3302843873@qq.com>
2026-06-27 14:21:48 +0800

    compiler: parser: expr: array_access: unit tests

--- compiler/src/parsing/expr/array_access.rs
+++ compiler/src/parsing/expr/array_access.rs
@@ -12,3 +12,55 @@ pub(super) fn array_access<'a>(expr: parser_type!(Expression)) -> parser_type!(E
             index: Box::new(index),
         })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        ast::expression::{BinaryOpKind, Expression},
+        parsing::{expr::expr, test_ident, test_parse, test_tokens},
+    };
+
+    #[test]
+    fn parses_array_access_expr() {
+        let tokens = test_tokens![
+            Token::Ident("items".into()),
+            Token::LeftSquareBracket,
+            Token::Number(0),
+            Token::RightSquareBracket,
+        ];
+
+        assert_eq!(
+            test_parse(array_access(expr()), &tokens),
+            Expression::ArrayAccess {
+                array: test_ident("items"),
+                index: Expression::Number(0).into(),
+            }
+        );
+    }
+
+    #[test]
+    fn parses_array_access_expr_with_expr_index() {
+        let tokens = test_tokens![
+            Token::Ident("items".into()),
+            Token::LeftSquareBracket,
+            Token::Number(1),
+            Token::Add,
+            Token::Number(2),
+            Token::RightSquareBracket,
+        ];
+
+        assert_eq!(
+            test_parse(array_access(expr()), &tokens),
+            Expression::ArrayAccess {
+                array: test_ident("items"),
+                index: Expression::BinaryOp {
+                    lhs: Expression::Number(1).into(),
+                    kind: BinaryOpKind::Add,
+                    rhs: Expression::Number(2).into(),
+                }
+                .into(),
+            }
+        );
+    }
+}
