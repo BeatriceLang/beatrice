4460a076a72947f3e3aaa7f234e773388e29aea4
elysia <3302843873@qq.com>
2026-06-27 13:45:03 +0800

    compiler: parser: expr: create_array: unit tests

--- compiler/src/parsing/expr/create_array.rs
+++ compiler/src/parsing/expr/create_array.rs
@@ -1,5 +1,4 @@
 use chumsky::{IterParser, Parser, primitive::just};
-use tap::TapFallible;
 
 use crate::{ast::expression::Expression, lexing::token::Token};
 
@@ -10,4 +9,66 @@ pub(super) fn create_array<'a>(expr: parser_type!(Expression)) -> parser_type!(E
         .ignore_then(elements)
         .then_ignore(just(Token::RightSquareBracket))
         .map(|elements| Expression::CreateArray(elements.into_iter().map(Box::new).collect()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        ast::expression::{BinaryOpKind, Expression},
+        parsing::{expr::expr, test_ident, test_parse, test_tokens},
+    };
+
+    #[test]
+    fn parses_empty_array_expression() {
+        let tokens = test_tokens![Token::LeftSquareBracket, Token::RightSquareBracket];
+
+        assert_eq!(
+            test_parse(create_array(expr()), &tokens),
+            Expression::CreateArray(vec![])
+        );
+    }
+
+    #[test]
+    fn parses_array_expression_with_single_element() {
+        let tokens = test_tokens![
+            Token::LeftSquareBracket,
+            Token::Number(42),
+            Token::RightSquareBracket,
+        ];
+
+        assert_eq!(
+            test_parse(create_array(expr()), &tokens),
+            Expression::CreateArray(vec![Expression::Number(42).into()])
+        );
+    }
+
+    #[test]
+    fn parses_array_expression_with_multiple_elements() {
+        let tokens = test_tokens![
+            Token::LeftSquareBracket,
+            Token::Number(1),
+            Token::Comma,
+            Token::Ident("x".into()),
+            Token::Comma,
+            Token::Number(2),
+            Token::Add,
+            Token::Number(3),
+            Token::RightSquareBracket,
+        ];
+
+        assert_eq!(
+            test_parse(create_array(expr()), &tokens),
+            Expression::CreateArray(vec![
+                Expression::Number(1).into(),
+                Expression::Ident(test_ident("x")).into(),
+                Expression::BinaryOp {
+                    lhs: Expression::Number(2).into(),
+                    kind: BinaryOpKind::Add,
+                    rhs: Expression::Number(3).into(),
+                }
+                .into(),
+            ])
+        );
+    }
 }
