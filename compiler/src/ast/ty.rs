26154fc7849c0826d5a8cd5cedd0d7a66f6f5e41
elysia <3302843873@qq.com>
2026-06-27 12:02:35 +0800

    compiler: ast: ty: array: size: usize -> u32

--- compiler/src/ast/ty.rs
+++ compiler/src/ast/ty.rs
@@ -6,7 +6,7 @@ pub enum Type {
     Bool,
     Ptr(Box<Self>),
     Struct(String),
-    Array { element_ty: Box<Self>, size: usize },
+    Array { element_ty: Box<Self>, size: u32 },
 }
 
 impl Type {
