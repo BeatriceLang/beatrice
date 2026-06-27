3b55a3ecc416de19fef2782a1e3327702dd5d760
elysia <3302843873@qq.com>
2026-06-27 11:21:35 +0800

    compiler: parser: expr: invert: unit tests

--- compiler/src/parsing/expr/invert.rs
+++ compiler/src/parsing/expr/invert.rs
@@ -7,3 +7,38 @@ pub(super) fn invert<'a>(expr: parser_type!(Expression)) -> parser_type!(Express
         .ignore_then(expr)
         .map(|val| Expression::Invert(Box::new(val)))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        ast::expression::Expression,
+        parsing::{expr::expr, test_ident, test_parse, test_tokens},
+    };
+
+    #[test]
+    fn parses_invert_expr() {
+        let tokens = test_tokens![Token::ExclamationMark, Token::Ident("value".into())];
+
+        assert_eq!(
+            test_parse(invert(expr()), &tokens),
+            Expression::Invert(Expression::Ident(test_ident("value")).into())
+        );
+    }
+
+    #[test]
+    fn parses_nested_invert_expr() {
+        let tokens = test_tokens![
+            Token::ExclamationMark,
+            Token::ExclamationMark,
+            Token::Ident("value".into()),
+        ];
+
+        assert_eq!(
+            test_parse(invert(expr()), &tokens),
+            Expression::Invert(
+                Expression::Invert(Expression::Ident(test_ident("value")).into()).into()
+            )
+        );
+    }
+}
