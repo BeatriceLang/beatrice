0cec37bc92c4c81cf92d2fe5d5426fc90bb2942d
elysia <3302843873@qq.com>
2026-06-30 21:22:43 +0800

    compiler: parser: item: type_alias: unit tests

--- compiler/src/parsing/item/type_alias.rs
+++ compiler/src/parsing/item/type_alias.rs
@@ -13,3 +13,46 @@ pub(super) fn type_alias<'a>() -> parser_type!(Item) {
         .then(ty())
         .map(|(alias, ty)| Item::TypeAlias { alias, ty })
 }
+
+#[cfg(test)]
+mod tests {
+    use chumsky::Parser as _;
+
+    use super::*;
+    use crate::{
+        ast::ty::Type,
+        parsing::{test_ident, test_input, test_parse, test_tokens},
+    };
+
+    #[test]
+    fn parses_type_alias_item() {
+        let tokens = test_tokens![
+            Token::Type,
+            Token::Ident("Count".into()),
+            Token::Assign,
+            Token::I32,
+        ];
+
+        assert_eq!(
+            test_parse(type_alias(), &tokens),
+            Item::TypeAlias {
+                alias: test_ident("Count"),
+                ty: Type::I32,
+            }
+        );
+    }
+
+    #[test]
+    fn rejects_type_alias_without_assign() {
+        let tokens = test_tokens![
+            Token::Type,
+            Token::Ident("Count".into()),
+            Token::I32,
+        ];
+
+        let errors = type_alias().parse(test_input(&tokens)).into_errors();
+
+        assert_eq!(errors.len(), 1);
+        assert_eq!(errors[0].span().into_range(), 2..3);
+    }
+}
